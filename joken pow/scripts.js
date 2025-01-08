bbd534410c69dc1a1c96bbfb750cb55fdc32c0e7
Saymon mello <saymon.mello1509@gmail.com>
2025-01-08 14:07:19 -0300

    new update-project

--- joken pow/scripts.js
+++ joken pow/scripts.js
@@ -6,9 +6,22 @@ const userResult = document.querySelector('.user-result img')
 
 const computerSrcImages = ['imagens/pedra.png','imagens/papel.png','imagens/tesoura.png']
 
+
+const winner = {
+    RR: 'Empate',
+    RP: 'Você',
+    RS: 'Computador',
+    PP: 'Empate',
+    PR: 'Você',
+    PS: 'Computador',
+    SS: 'Empate',
+    SR: 'Computador',
+    SP: 'Você'
+}
+
 function handleOptionClick(event) {
     const clickedImage = event.currentTarget
-    const clickedIndex = Array.from(optionImage).indexOf(clickedImage)
+    const userIndex = Array.from(optionImage).indexOf(clickedImage)
     
 
     container.classList.add('start')
@@ -19,10 +32,17 @@ function handleOptionClick(event) {
     setTimeout(() => {
         container.classList.remove('start')
 
-        userResult.src  =computerSrcImages[clickedIndex]
+        userResult.src  =computerSrcImages[userIndex]
 
         const randomNumber =  Math.floor(Math.random() * computerSrcImages.length)
         computerResult.src = computerSrcImages[randomNumber]
+
+
+        const userValue = ['R', 'P', 'S'][userIndex]
+        const computerValue = ['R', 'P', 'S'][randomNumber]
+        const finalResult = userValue + computerValue
+
+        console.log(finalResult)
 
         resultText.innerHTML = 'X ganhou'
         
